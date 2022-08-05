ef32ecc4869e938e98482743ddcdebefd22a9e66
Victor Bo <10667379@qq.com>
2022-08-05 12:36:11 +0800

    chore: fix publish fail

--- packages/index.ts
+++ packages/index.ts
@@ -1,21 +1,21 @@
 import array from './core/array/entry'
 import color from './core/color/entry'
-import is from './core/is/entry'
 import fn from './core/fn/entry'
+import is from './core/is/entry'
 import string from './core/string/entry'
 
 export {
   array,
   color,
-  is,
   fn,
+  is,
   string,
 }
 
 export default {
   array,
   color,
-  is,
   fn,
+  is,
   string,
 }
