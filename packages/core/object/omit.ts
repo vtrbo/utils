c4d741b55f81bc947abbcb8f1590afa1c45db7ef
Victor Bo <10667379@qq.com>
2022-08-10 18:25:58 +0800

    chore: fix object pick omit type error

--- packages/core/object/omit.ts
+++ packages/core/object/omit.ts
@@ -9,7 +9,7 @@ import { keys as objectKeys } from './keys'
  * @returns { Omit<O, T> }
  */
 export function omit<O, T extends keyof O>(object: O, keys: T[]): Omit<O, T> {
-  return objectKeys(object as object).reduce((acc, key) => {
+  return objectKeys(object as unknown as object).reduce((acc, key) => {
     return keys.includes(key) ? acc : { ...acc, [key]: object[key] }
   }, {} as Omit<O, T>)
 }
--- packages/core/object/pick.ts
+++ packages/core/object/pick.ts
@@ -9,7 +9,7 @@ import { keys as objectKeys } from './keys'
  * @returns { Pick<O, T> }
  */
 export function pick<O, T extends keyof O>(object: O, keys: T[]): Pick<O, T> {
-  return objectKeys(object as object).reduce((acc, key) => {
+  return objectKeys(object as unknown as object).reduce((acc, key) => {
     return keys.includes(key) ? { ...acc, [key]: object[key] } : acc
   }, {} as Pick<O, T>)
 }
