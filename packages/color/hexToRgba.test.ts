5a709243285c3b5ae95d0fb545ce0275956d75e4
Victor Bo <10667379@qq.com>
2023-01-28 17:41:28 +0800

    chore: fix hexToRgba length = 4 error

--- packages/color/hexToRgba.test.ts
+++ packages/color/hexToRgba.test.ts
@@ -2,8 +2,39 @@ import { expect, it } from 'vitest'
 import { hexToRgba } from './hexToRgba'
 
 it('hexToRgba', () => {
-  expect(hexToRgba('#ffffff')).toEqual([255, 255, 255])
-  expect(hexToRgba('#000')).toEqual([0, 0, 0])
-  expect(hexToRgba('#24eaa800')).toEqual([36, 234, 168, 0])
-  expect(hexToRgba('#ff008b59')).toEqual([255, 0, 139, 0.35])
+  expect(hexToRgba('#0')).toMatchInlineSnapshot('undefined')
+  expect(hexToRgba('#00')).toMatchInlineSnapshot('undefined')
+  expect(hexToRgba('#000')).toMatchInlineSnapshot(`
+    [
+      0,
+      0,
+      0,
+    ]
+  `)
+  expect(hexToRgba('#0006')).toMatchInlineSnapshot(`
+    [
+      0,
+      0,
+      0,
+      0.02,
+    ]
+  `)
+  expect(hexToRgba('#fffff')).toMatchInlineSnapshot('undefined')
+  expect(hexToRgba('#ffffff')).toMatchInlineSnapshot(`
+    [
+      255,
+      255,
+      255,
+    ]
+  `)
+  expect(hexToRgba('#24eaa80')).toMatchInlineSnapshot('undefined')
+  expect(hexToRgba('#ff008b59')).toMatchInlineSnapshot(`
+    [
+      255,
+      0,
+      139,
+      0.35,
+    ]
+  `)
+  expect(hexToRgba('#ff008b598')).toMatchInlineSnapshot('undefined')
 })
--- packages/color/rgbaToHex.test.ts
+++ packages/color/rgbaToHex.test.ts
@@ -2,11 +2,11 @@ import { expect, it } from 'vitest'
 import { rgbaToHex } from './rgbaToHex'
 
 it('rgbaToHex', () => {
-  expect(rgbaToHex(255, 255, 255)).toEqual('#ffffff')
-  expect(rgbaToHex(36, 234, 168, 0)).toEqual('#24eaa800')
-  expect(rgbaToHex(255, 0, 139, 0.35)).toEqual('#ff008b59')
-  expect(rgbaToHex(0, 255, 36, 0.5)).toEqual('#00ff247f')
-  expect(rgbaToHex(0, 0, 0, 0.78)).toEqual('#000000c6')
-  expect(rgbaToHex(217, 8, 16, 1)).toEqual('#d90810ff')
+  expect(rgbaToHex(255, 255, 255)).toMatchInlineSnapshot('"#ffffff"')
+  expect(rgbaToHex(36, 234, 168, 0)).toMatchInlineSnapshot('"#24eaa800"')
+  expect(rgbaToHex(255, 0, 139, 0.35)).toMatchInlineSnapshot('"#ff008b59"')
+  expect(rgbaToHex(0, 255, 36, 0.5)).toMatchInlineSnapshot('"#00ff247f"')
+  expect(rgbaToHex(0, 0, 0, 0.78)).toMatchInlineSnapshot('"#000000c6"')
+  expect(rgbaToHex(217, 8, 16, 1)).toMatchInlineSnapshot('"#d90810ff"')
 })
 
