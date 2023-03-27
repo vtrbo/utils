3d54e7ebba10d6987332a48e53d5a31ba303968b
Victor Bo <10667379@qq.com>
2023-03-27 09:38:38 +0800

    chore: update runcode plugin

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,11 @@
 import theme from 'vitepress/theme'
-import RunCode from 'vitepress-plugin-runcode'
+import VppRunCode from 'vitepress-plugin-runcode'
 import 'vitepress-plugin-runcode/styles.css'
 import './index.css'
 
 export default {
   ...theme,
   enhanceApp({ app }) {
-    app.use(RunCode)
+    app.use(VppRunCode)
   },
 }
