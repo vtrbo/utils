fc2b9f6fd6906cc9cc1bebb12c2f4acdb0968dbe
Victor Bo <hi@vtrbo.cn>
2023-10-16 12:25:49 +0800

    feat: added utils-obj package (#13)
    * ci: modify project structure
    
    * feat: added utils-obj package

--- packages/log/index.ts
+++ packages/log/index.ts
@@ -1,2 +1,2 @@
-export * from './src/types'
+export * from './types'
 export * from './src/log'
--- packages/tool/src/tool.ts
+++ packages/tool/src/tool.ts
@@ -1,3 +1,7 @@
 export function toRawType(data: any): string {
   return Object.prototype.toString.call(data).slice(8, -1)
 }
+
+export function notNullish<T>(v: T | null | undefined): v is NonNullable<T> {
+  return v != null
+}
