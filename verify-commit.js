6e9afb70c7b84dfced7d83a26bf169b82135dca9
Victor Bo <hi@vtrbo.cn>
2023-10-17 09:32:20 +0800

    ci: added prepare script

--- verify-commit.js
+++ verify-commit.js
@@ -6,11 +6,10 @@ import { log } from './packages/core'
 const msgPath = path.resolve('.git/COMMIT_EDITMSG')
 const msg = readFileSync(msgPath, 'utf-8').trim()
 
-const commitKeys = 'feat|fix|perf|refactor|test|types|docs|style|ci|build|release|workflow|dx|chore|wip|update|optimizate'
-const commitRE = new RegExp(`^(revert: )?(${commitKeys})(\\(.+\\))?: .{1,50}`)
+const commitRE = /^(revert: )?(feat|fix|perf|refactor|test|types|docs|style|ci|build|release|workflow|dx|chore|wip|update|optimizate)(\\(.+\\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
   log.error('commit format error')
-  log.error(commitKeys)
+  log.error('feat|fix|perf|refactor|test|types|docs|style|ci|build|release|workflow|dx|chore|wip|update|optimizate')
   process.exit(1)
 }
