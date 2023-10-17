f0563deb087ccb070dde82414d0a94cd5e5d4d7e
Victor Bo <hi@vtrbo.cn>
2023-10-17 10:34:47 +0800

    ci: added package keywords

--- verify-commit.js
+++ verify-commit.js
@@ -8,6 +8,6 @@ const msg = fs.readFileSync(msgPath, 'utf-8').trim()
 const commitRE = /^(revert: )?(feat|fix|perf|refactor|test|types|docs|style|ci|build|release|workflow|dx|chore|wip|update|optimizate)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
-  console.log('无效的提交')
+  console.log('invalid commit message format')
   process.exit(1)
 }
