f07ebf4ce3d415c473bc45ae34a6e9b9bc2adf3b
Victor Bo <hi@vtrbo.cn>
2023-10-17 10:25:03 +0800

    ci: fixed eslint error or warn

--- verify-commit.js
+++ verify-commit.js
@@ -1,15 +1,13 @@
-import { readFileSync } from 'node:fs'
+import fs from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
-import { log } from './packages/core'
 
 const msgPath = path.resolve('.git/COMMIT_EDITMSG')
-const msg = readFileSync(msgPath, 'utf-8').trim()
+const msg = fs.readFileSync(msgPath, 'utf-8').trim()
 
-const commitRE = /^(revert: )?(feat|fix|perf|refactor|test|types|docs|style|ci|build|release|workflow|dx|chore|wip|update|optimizate)(\\(.+\\))?: .{1,50}/
+const commitRE = /^(revert: )?(feat|fix|perf|refactor|test|types|docs|style|ci|build|release|workflow|dx|chore|wip|update|optimizate)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
-  log.error('commit format error')
-  log.error('feat|fix|perf|refactor|test|types|docs|style|ci|build|release|workflow|dx|chore|wip|update|optimizate')
+  console.log('无效的提交')
   process.exit(1)
 }
