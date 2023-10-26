a538566ef64f8b60f04db5d27ee6f60b324b345d
Victor Bo <hi@vtrbo.cn>
2023-10-26 08:59:06 +0800

    feat: added arr fns

--- packages/arr/src/arr.ts
+++ packages/arr/src/arr.ts
@@ -1,3 +1,16 @@
-export function fn() {
-  return ''
+import { isArray } from '@vtrbo/utils-is'
+
+export function toArr<T>(data?: T | T[]): T[] {
+  data = data ?? []
+  return isArray(data) ? data : [data]
+}
+
+export function groupBy<T>(list: T[], fn: (single: T) => any): Map<string, T[]> {
+  const map = new Map<string, T[]>()
+  list.forEach((s: T) => {
+    const key = fn(s)
+    map.set(key, map.get(key) || [])
+    map.get(key)!.push(s)
+  })
+  return map
 }
