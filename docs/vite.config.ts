653f8f31bea78336440ac33b9be14c21047b65f9
Victor Bo <hi@vtrbo.cn>
2023-10-30 15:09:12 +0800

    fix: fixed deploy docs error (#44)

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 export default defineConfig({
   plugins: [
     UnoCSS(),
+    viteExternalsPlugin({
+      typescript: 'ts',
+    }),
   ],
   build: {
     chunkSizeWarningLimit: 999999,
