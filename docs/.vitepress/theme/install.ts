d55f869e5c7bf63ff0b36f77ace6a7e53070f84a
Victor Bo <10667379@qq.com>
2023-11-21 12:02:51 +0800

    fix: fixed build docs error

--- docs/.vitepress/theme/install.ts
+++ docs/.vitepress/theme/install.ts
@@ -2,6 +2,7 @@ import * as ARR from '@vtrbo/utils-arr'
 import * as COLOR from '@vtrbo/utils-color'
 import * as IS from '@vtrbo/utils-is'
 import * as LOG from '@vtrbo/utils-log'
+import * as OBJ from '@vtrbo/utils-obj'
 import * as STR from '@vtrbo/utils-str'
 import * as TOOL from '@vtrbo/utils-tool'
 import * as TREE from '@vtrbo/utils-tree'
@@ -10,6 +11,7 @@ import * as TREE from '@vtrbo/utils-tree'
 (globalThis as any)['@vtrbo/utils-color'] = COLOR;
 (globalThis as any)['@vtrbo/utils-is'] = IS;
 (globalThis as any)['@vtrbo/utils-log'] = LOG;
+(globalThis as any)['@vtrbo/utils-obj'] = OBJ;
 (globalThis as any)['@vtrbo/utils-str'] = STR;
 (globalThis as any)['@vtrbo/utils-tool'] = TOOL;
 (globalThis as any)['@vtrbo/utils-tree'] = TREE
