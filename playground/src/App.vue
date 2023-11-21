dc2c90ce807c4e5c750ad353c58620e5fe7b3047
Victor Bo <10667379@qq.com>
2023-11-21 14:39:54 +0800

    fix: fixed build docs error

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
 import { isColor } from '@vtrbo/utils'
+import { toArray } from '@vtrbo/utils-arr'
+import { isArray } from '@vtrbo/utils-is'
+
+console.log(toArray('123'), isArray('acb'))
 </script>
 
 <template>
